d25e3c91a4fcf0ad4688d8c8486d2667213d6007
Harry <hello@majored.pw>
2022-10-09 16:51:14 +0100

    Addition of doc comments for HashedReader & poll_result_ok!

--- src/read/io/hashed.rs
+++ src/read/io/hashed.rs
@@ -10,6 +10,7 @@ use tokio::io::{AsyncRead, ReadBuf};
 use crc32fast::Hasher;
 use pin_project::pin_project;
 
+/// A wrapping reader which computes the CRC32 hash of data read via [`AsyncRead`].
 #[pin_project]
 pub(crate) struct HashedReader<R> where R: AsyncRead + Unpin {
     #[pin]
@@ -18,14 +19,24 @@ pub(crate) struct HashedReader<R> where R: AsyncRead + Unpin {
 }
 
 impl<R> HashedReader<R> where R: AsyncRead + Unpin {
+    /// Constructs a new wrapping reader from a generic [`AsyncRead`] implementer.
     pub(crate) fn new(reader: R) -> Self {
         Self { reader, hasher: Hasher::default() }
     }
 
+    /// Consumes this reader and returns the computed CRC32 hash.
+    /// 
+    /// This method is consuming as the internal hasher also requires consuming in order to compute the hash. See the
+    /// non-consuming counterpart, swap_and_compute_hash(), as an alternative.
     pub(crate) fn compute_hash(self) -> u32 {
         self.hasher.finalize()
     }
 
+    /// Swaps the internal hasher and returns the computed CRC32 hash.
+    /// 
+    /// The internal hasher is taken and replaced with a newly-constructed one. As a result, this method should only be
+    /// called once EOF has been reached and it's known that no more data will be read, else the computed hash(s) won't
+    /// accurately represent the data read in.
     pub(crate) fn swap_and_compute_hash(&mut self) -> u32 {
         std::mem::take(&mut self.hasher).finalize()
     }
--- src/read/io/mod.rs
+++ src/read/io/mod.rs
@@ -1,6 +1,10 @@
 pub(crate) mod compressed;
 pub(crate) mod hashed;
 
+/// A macro that returns the inner value of an Ok or early-returns in the case of an Err.
+/// 
+/// This is almost identical to the ? operator but handles the situation when a Result is used in combination with
+/// Poll (eg. tokio's IO traits such as AsyncRead).
 macro_rules! poll_result_ok {
     ($poll:expr) => {
         match $poll {
