8ef2efb409306094f24c310188ae32edfab3a341
Harry <hello@majored.pw>
2022-10-09 16:29:49 +0100

    Addition of macro to short circuit when an IO future poll returns Err

--- src/read/io/mod.rs
+++ src/read/io/mod.rs
@@ -1,1 +1,13 @@
-pub(crate) mod compression;
+pub(crate) mod compressed;
+pub(crate) mod hashed;
+
+macro_rules! poll_result_ok {
+    ($poll:expr) => {
+        match $poll {
+            Ok(inner) => inner,
+            Err(err) => return Poll::Ready(Err(err)),
+        }
+    };
+}
+
+use poll_result_ok;
