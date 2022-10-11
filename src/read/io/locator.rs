a05785193440b90bbeda677154e007063df97080
Harry <hello@majored.pw>
2022-10-11 14:11:43 +0100

    Addition of reverse_search_buffer (modified from async-io-utilities)

--- src/read/io/locator.rs
+++ src/read/io/locator.rs
@@ -31,3 +31,76 @@ where
 
     Ok(locations)
 }
+
+/// A type which holds data about a match within 'reverse_search_buffer()'.
+/// 
+/// The 'match_index' field indicates where the match starts with respects to the reverse order.
+/// Ie. the match occurs at indexes <= match_index.
+struct SignatureMatch {
+    full_match: bool,
+    match_index: usize,
+}
+
+// A naive reverse linear search along the buffer for the specified signature bytes.
+fn reverse_search_buffer(buffer: &[u8], signature: &[u8]) -> Option<SignatureMatch> {
+    // This is already surprisingly performant. For instance, using memchr::memchr() to match for the first byte of the
+    // signature, and then manual byte comparisons for the remaining signature bytes was actually slower by a factor of
+    // 2.25. This method was explored as tokio's `read_until()` implementation uses memchr::memchr().
+
+    'outer: for index in (0..buffer.len()).rev() {
+        for (signature_index, signature_byte) in signature.iter().rev().enumerate() {
+            let index_to_check = index.checked_sub(signature_index);
+
+            if index_to_check.is_none() && signature_index != 0 { 
+                // We have a partial match but have hit the start of the buffer.
+                return Some(SignatureMatch { full_match: false, match_index: index });
+            }
+
+            if buffer[index_to_check.unwrap()] != *signature_byte {
+                continue 'outer;
+            }
+        }
+
+        return Some(SignatureMatch { full_match: true, match_index: index });
+    }
+
+    None
+}
+
+#[cfg(test)]
+#[test]
+fn search_one_byte_test() {
+    let buffer: &[u8] = &[0x0, 0x0, 0x0, 0x0, 0x0, 0x0];
+    let signature: &[u8] = &[0x1];
+
+    let matched = reverse_search_buffer(buffer, signature);
+    assert!(matched.is_none());
+
+    let buffer: &[u8] = &[0x2, 0x1, 0x0, 0x0, 0x0, 0x0];
+    let signature: &[u8] = &[0x1];
+
+    let matched = reverse_search_buffer(buffer, signature);
+    assert!(matched.is_some());
+    assert!(matched.as_ref().unwrap().full_match);
+    assert_eq!(1, matched.as_ref().unwrap().match_index);
+}
+
+#[cfg(test)]
+#[test]
+fn search_two_byte_test() {
+    let buffer: &[u8] = &[0x2, 0x1, 0x0, 0x0, 0x0, 0x0];
+    let signature: &[u8] = &[0x2, 0x1];
+
+    let matched = reverse_search_buffer(buffer, signature);
+    assert!(matched.is_some());
+    assert!(matched.as_ref().unwrap().full_match);
+    assert_eq!(1, matched.as_ref().unwrap().match_index);
+
+    let buffer: &[u8] = &[0x1, 0x0, 0x0, 0x0, 0x0, 0x0];
+    let signature: &[u8] = &[0x2, 0x1];
+
+    let matched = reverse_search_buffer(buffer, signature);
+    assert!(matched.is_some());
+    assert!(!matched.as_ref().unwrap().full_match);
+    assert_eq!(0, matched.as_ref().unwrap().match_index);
+}
