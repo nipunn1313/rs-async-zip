c3010622027589950eaabea4da13787b16b1f2e9
Harry <hello@majored.pw>
2022-10-14 02:56:33 +0100

    Fix comment types & remove partial match logic from `reverse_search_buffer()`

--- src/read/io/locator.rs
+++ src/read/io/locator.rs
@@ -36,16 +36,13 @@ const EOCDR_LOWER_BOUND: u64 = EOCDR_UPPER_BOUND + SIGNATURE_LENGTH as u64 + u16
 
 /// Locate the `end of central directory record` offset, if one exists.
 /// 
-/// This method involves buffered reading in reverse and revese linear searching along those buffers for the EOCDR
-/// signature. As a result of this buffered approach, we reduce seeks when compared to `zip-rs`'s by a factor of
-/// the buffer size. We also then don't have to do individual u32 reads against the upstream reader.
+/// This method involves buffered reading in reverse and reverse linear searching along those buffers for the EOCDR
+/// signature. As a result of this buffered approach, we reduce seeks when compared to `zip-rs`'s method by a factor
+/// of the buffer size. We also then don't have to do individual u32 reads against the upstream reader.
 /// 
-/// Whilst I haven't done any in-depth benchmarks, when reading a ZIP file with the maximum length comment this method
+/// Whilst I haven't done any in-depth benchmarks, when reading a ZIP file with the maximum length comment, this method
 /// saw a reduction in location time by a factor of 500 when compared with the `zip-rs` method.
-pub(crate) async fn eocdr<R>(mut reader: R) -> Result<u64>
-where 
-    R: AsyncRead + AsyncSeek + Unpin,
-{
+pub(crate) async fn eocdr<R>(mut reader: R) -> Result<u64> where  R: AsyncRead + AsyncSeek + Unpin {
     let length = reader.seek(SeekFrom::End(0)).await?;
     let signature = &EOCDR_SIGNATURE.to_le_bytes();
     let mut buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
@@ -81,21 +78,16 @@ where
 fn reverse_search_buffer(buffer: &[u8], signature: &[u8]) -> Option<usize> {
     'outer: for index in (0..buffer.len()).rev() {
         for (signature_index, signature_byte) in signature.iter().rev().enumerate() {
-            let index_to_check = index.checked_sub(signature_index);
-
-            // We can ignore the partial match case due to the reasons described above.
-            if index_to_check.is_none() && signature_index != 0 {
+            if let Some(next_index) =  index.checked_sub(signature_index) {
+                if buffer[next_index] != *signature_byte {
+                    continue 'outer;
+                }
+            } else {
                 break 'outer;
             }
-
-            if buffer[index_to_check.unwrap()] != *signature_byte {
-                continue 'outer;
-            }
         }
-
         return Some(index);
     }
-
     None
 }
 
